377beff865f70c4811109a69622da8a50540099a
0Jsnipes <jared.c.snipes@gmail.com>
2024-09-23 16:25:46 -0400

    done

--- api/auth/users-model.js
+++ api/auth/users-model.js
@@ -4,15 +4,7 @@ function add(user) {
     return db('users')
       .insert(user)
       .returning('*') // This returns all columns of the inserted row
-      .then(([newUser]) => {
-        // Destructure the newUser and convert id to string
-        const { id , username, password } = newUser; // Destructure properties
-        return {
-          id: id, // Convert id to string
-          username: username,
-          password: password // Optionally, you might want to omit the password in the response
-        };
-      });
+      .then(([user]))
   }
   
 function findBy(filter) {
