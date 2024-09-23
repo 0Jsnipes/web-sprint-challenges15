805fdfa15ea4ef6d86fc1bfadea72a1a4e1d07fd
0Jsnipes <jared.c.snipes@gmail.com>
2024-09-23 16:56:40 -0400

    done

--- api/auth/users-model.js
+++ api/auth/users-model.js
@@ -1,11 +1,27 @@
 const db = require('../../data/dbConfig'); // Assuming you have a dbConfig for database connection
 
+// users-model.js
+// Adjust based on your database setup
+
 function add(user) {
-    return db('users')
-      .insert(user)
-      .returning('*') // This returns all columns of the inserted row
-      .then(([newUser])=> newUser)
-  }
+  return db('users')
+    .insert(user) // Insert the user into the database
+    .returning('*') // Return all columns of the newly inserted row
+    .then(([newUser]) => {
+      return {
+        id: newUser, // Ensure id is returned as a string
+         // Include the username
+        // You can choose not to return the password for security reasons
+      };
+    });
+}
+
+module.exports = {
+  add,
+  findBy,
+  // ...other methods
+};
+
   
 function findBy(filter) {
   return db('users').where(filter);
